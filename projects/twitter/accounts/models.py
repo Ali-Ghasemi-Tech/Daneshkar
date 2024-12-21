8fc4ccb0dbc2cdbd7b688d162db1ff9dfcd2d577
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2024-12-21 15:38:57 +0330

    make custome user with bio and picture

--- projects/twitter/accounts/models.py
+++ projects/twitter/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomeUserModel(AbstractUser):
+    bio = models.TextField(max_length= 500 , null=True , blank=True)
+    picture = models.FileField(blank=True)
--- projects/twitter/accounts/views.py
+++ projects/twitter/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import CreateView , UpdateView
-from django.contrib.auth.forms import UserCreationForm , UserChangeForm
+from django.contrib.auth.forms import UserCreationForm 
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from .models import CustomeUserModel
 # Create your views here.
 
 class SignUpView(CreateView):
@@ -11,10 +12,10 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
 
 class ProfileView(UpdateView):
-    model = User
+    model = CustomeUserModel
     template_name = 'profile.html'
     success_url = reverse_lazy('profile')
-    fields = ['username' , 'first_name' , 'last_name', 'email']
+    fields = ['username' , 'first_name' , 'last_name', 'email' , 'bio' , 'picture']
 
 
     def get_object(self): 
